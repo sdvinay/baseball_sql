4cdf9ad66e25318f3b498dbbc470d8bf22f295a5
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-05-08 18:39:14 -0700

    Code standardization/beautification

--- no_hitters_broken_up.sql
+++ no_hitters_broken_up.sql
@@ -5,21 +5,21 @@
 
 drop table t_first_hits;
 create table t_first_hits as 
-	select game_id, bat_home_id, event_id, inn_ct
-	from  # find the first hit of every team-game by using a window for each team-game
-	(	select game_id, bat_home_id, event_id, inn_ct, min(event_id) over 
-			(partition by game_id, bat_home_id) as first_hit
-		from event 
-		where  game_id in (select game_id from retrosheet.game where year(game_dt)>1973) 
-			and h_fl>0  # h_fl is actually total bases, not a flag
-		) hits  # this sub-query will return *all* hits, with a field for first_hit
-where event_id=first_hit
+    select game_id, bat_home_id, event_id, inn_ct
+    from  # find the first hit of every team-game by using a window for each team-game
+        (select game_id, bat_home_id, event_id, inn_ct, min(event_id) over 
+            (partition by game_id, bat_home_id) as first_hit
+        from event 
+        where  game_id in (select game_id from retrosheet.game where year(game_dt)>1973) 
+            and h_fl>0  # h_fl is actually total bases, not a flag
+        ) hits  # this sub-query will return *all* hits, with a field for first_hit
+    where event_id=first_hit
 ;
 
-# create a temp table for pitchers who have blown a no-hitter in the 9th
+# create a temp table for no-hitters blown in the 9th (including the pitcher giving up the hit)
 drop table t_nonos_blown;
 create table t_nonos_blown as 
-	select pit_id, t_first_hits.game_id as game_id 
+    select pit_id, t_first_hits.game_id as game_id 
     from event inner join t_first_hits
     on event.game_id=t_first_hits.game_id and event.event_id = t_first_hits.event_id
     where t_first_hits.inn_ct>8
@@ -28,7 +28,7 @@ create table t_nonos_blown as
 # summarize and format the pitchers that have blown multiple
 select name_first, name_last, num, least_recent, most_recent 
 from  # group by pitcher, and get the dates from retrosheet.game
-	(select pit_id, min(game_dt) as least_recent, max(game_dt) as most_recent, count(*) as num   
+    (select pit_id, min(game_dt) as least_recent, max(game_dt) as most_recent, count(*) as num   
     from t_nonos_blown inner join retrosheet.game 
     on t_nonos_blown.game_id=retrosheet.game.game_id   
     group by pit_id 
@@ -36,6 +36,6 @@ from  # group by pitcher, and get the dates from retrosheet.game
 inner join baseballdatabank.people # get the first and last names
 on baseballdatabank.people.retro_id=x.pit_id 
 where num>1 
-order by num desc, most_recent, name_last;
-
+order by num desc, most_recent, name_last
+;
 
