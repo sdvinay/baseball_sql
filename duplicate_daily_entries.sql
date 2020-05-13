41e200b44618c2e5cc1487448f3a79e56d781b60
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-05-13 15:11:35 -0700

    Find duplicate entries in retrosheet.daily

---
+++ duplicate_daily_entries.sql
@@ -0,0 +1,12 @@
+
+drop table t_daily_dups;
+create table t_daily_dups as
+select * from
+(	select game_id, max(game_dt) as game_dt, count(*) as cnt from retrosheet.daily 
+	group by game_id, player_id) counts
+where cnt>1
+;
+describe t_daily_dups;
+select year(`max(game_dt)`) as yr, count(*) as num from t_daily_dups
+group by year(`max(game_dt)`)
+order by num desc;
