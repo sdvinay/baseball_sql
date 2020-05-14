35af671cb55a77e9c6bc8882ba9d7f21404f34bd
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-05-14 09:37:50 -0700

    simplified 'countif' expression

--- scoring_when_leadoff_reaches.sql
+++ scoring_when_leadoff_reaches.sql
@@ -15,7 +15,7 @@ select count(*) from t_event_1985_leadoffs;  # 26*162*9 = 37908
 
 select *, inn_scored/inn_total as freq_scored, runs_scored/inn_total as avg_scored, runs_scored/inn_scored as avg_runs_when_scored 
 from
-(	select  event_outs_ct=0 as reached, count(*) as inn_total, COUNT(NULLIF(0, (event_runs_ct+fate_runs_ct))) as inn_scored, 
+(	select  event_outs_ct=0 as reached, count(*) as inn_total, sum((event_runs_ct+fate_runs_ct)>0) as inn_scored, 
 		sum(event_runs_ct+fate_runs_ct) as runs_scored
 	from t_event_1985_leadoffs
 	group by event_outs_ct
