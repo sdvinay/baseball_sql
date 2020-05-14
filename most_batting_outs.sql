7c84d996900125ad7cc6594cca2c0483d74768d7
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-05-13 19:41:43 -0700

    Specified three different ways of counting outs

--- most_batting_outs.sql
+++ most_batting_outs.sql
@@ -1,5 +1,15 @@
 # Most batting outs in a season
 
-select *, ab-h+sh+sf+gidp as outs from baseballdatabank.batting 
-order by ab-h+sh+sf+gidp desc
-limit 20
+# AB-H is simple and consistent across baseball history, but missing some outs
+select *, ab-h as simple_outs from baseballdatabank.batting 
+order by simple_outs desc limit 20;
+
+# I think this is the best way to capture "batting outs" (would be better if we could subtract ROE)
+# but some categories are incomplete historically, so we have less certainty about the output
+select *, ab-h+sh+sf+gidp as batting_outs from baseballdatabank.batting 
+order by batting_outs desc limit 20;
+
+# baseball-reference includes CS in their "outs" leaders, but not other baserunning outs.  Meh.
+select *, ab-h+sh+sf+gidp+cs as baseball_reference_outs from baseballdatabank.batting 
+order by baseball_reference_outs desc limit 20;
+
