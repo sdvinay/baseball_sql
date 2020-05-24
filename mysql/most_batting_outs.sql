c4747956b73d4af9794650366b4042df5de1613a
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-05-24 03:41:33 -0700

    Cleaned up commenting

--- mysql/most_batting_outs.sql
+++ mysql/most_batting_outs.sql
@@ -1,15 +1,15 @@
-# Most batting outs in a season
+-- Most batting outs in a season
 
-# AB-H is simple and consistent across baseball history, but missing some outs
+-- AB-H is simple and consistent across baseball history, but missing some types of outs
 select *, ab-h as simple_outs from baseballdatabank.batting 
 order by simple_outs desc limit 20;
 
-# I think this is the best way to capture "batting outs" (would be better if we could subtract ROE)
-# but some categories are incomplete historically, so we have less certainty about the output
+-- I think this is the best way to capture "batting outs" (would be even better if we could subtract ROE)
+-- But some categories are incomplete historically, so comparing across eras may be problematic
 select *, ab-h+sh+sf+gidp as batting_outs from baseballdatabank.batting 
 order by batting_outs desc limit 20;
 
-# baseball-reference includes CS in their "outs" leaders, but not other baserunning outs.  Meh.
+-- baseball-reference includes CS in their "outs" leaders, but not other baserunning outs.  Meh.
 select *, ab-h+sh+sf+gidp+cs as baseball_reference_outs from baseballdatabank.batting 
 order by baseball_reference_outs desc limit 20;
 
