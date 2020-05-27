16a3660127ea65d02023219b27987cd11df1b080
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-05-27 10:46:53 -0700

    Code simplification

--- postgresql/multi_bases_loaded_pa_inning.sql
+++ postgresql/multi_bases_loaded_pa_inning.sql
@@ -3,40 +3,40 @@
 
 
 -- find all instances of multi bases-loaded PA in one inning
+-- this takes ~15 seconds, so create a temp table for these
+drop table t_multi_bl_pa;
 create table t_multi_bl_pa as
-with
-games as (select game_id, game_dt, away_team_id, home_team_id from retrosheet_game where extract(year from game_dt)>1900)
-select bl_multi.*, games.game_dt, games.away_team_id, games.home_team_id from 
+select bl_multi.*, game_dt, away_team_id, home_team_id from 
 (
 	select * from
 	(
-		select game_id, bat_home_id, inn_ct, bat_id, count(distinct event_id) as num, min(event_id) as first_event, max(event_id) as last_event
+		select game_id, bat_home_id, inn_ct, bat_id, count(distinct event_id) as num, 
+			min(event_id) as first_event, max(event_id) as last_event
 		from retrosheet_event 
-		where game_id in (select game_id from games) and start_bases_cd=7 -- start_bases_cd=7 means bases loaded
+		where start_bases_cd=7 -- start_bases_cd=7 means bases loaded
 		group by game_id, bat_home_id, inn_ct, bat_id
 	) bl_counts
 where num >1 and last_event-first_event>7  -- look for a gap of >7 in event_id, to get distinct PAs
 ) bl_multi
-inner join games on games.game_id = bl_multi.game_id;
+inner join retrosheet_game on retrosheet_game.game_id = bl_multi.game_id;
 
-
+-- how frequently does this happen?
 select * from t_multi_bl_pa order by game_dt;
-
 select * from t_multi_bl_pa where extract(year from game_dt)>=1999 order by game_dt;
 
 
 -- find players who had multiple bl_multi's in their career
 select name_first, name_last, num from 
 (
-select bat_id, count(*) as num from t_multi_bl_pa
-group by bat_id
+	select bat_id, count(*) as num from t_multi_bl_pa
+	group by bat_id
 ) by_batter
 inner join baseballdatabank_people
 on baseballdatabank_people.retro_id = by_batter.bat_id
 order by num desc;
 
 -- find bl_multi's where the batter homered in the first PA (actually, event)
-with t_gs_then_bl_in_inning as
+with gs_then_bl_in_inning as
 (
 	select t_multi_bl_pa.* 
 	from t_multi_bl_pa
@@ -47,8 +47,7 @@ with t_gs_then_bl_in_inning as
 select name_first, name_last, game_dt, inn_ct,
 	case when bat_home_id=true then home_team_id else away_team_id end as tm,
 	case when bat_home_id=true then away_team_id else home_team_id end as opp
-from t_gs_then_bl_in_inning
+from gs_then_bl_in_inning
 inner join baseballdatabank_people
-on baseballdatabank_people.retro_id = t_gs_then_bl_in_inning.bat_id
-order by game_dt
-;
+on baseballdatabank_people.retro_id = gs_then_bl_in_inning.bat_id
+order by game_dt;
