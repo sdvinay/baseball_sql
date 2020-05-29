5ef256371c8bb2edd91b1bec26dbc9ca9cf4ffab
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-05-28 20:23:38 -0700

    Fix whitespace (yes, really)

--- postgresql/3pas_in_an_inning.sql
+++ postgresql/3pas_in_an_inning.sql
@@ -17,12 +17,11 @@ superset as (select game_id, inn_ct, bat_id, event_id, bat_home_id, event_outs_c
             )
 
 select * -- and now narrow to the events that actually count in the 3PA innings 
-  from (
-		select min(event_id) over (partition by game_id, inn_ct, bat_id) as first_event,
-			   max(event_id) over (partition by game_id, inn_ct, bat_id) as last_event,
-			   superset.*
-		  from superset
-  ) as foo
+  from (  select min(event_id) over (partition by game_id, inn_ct, bat_id) as first_event,
+                 max(event_id) over (partition by game_id, inn_ct, bat_id) as last_event,
+                 superset.*
+            from superset
+       ) as foo
  where (last_event-first_event)>16
 ;
 
@@ -31,12 +30,12 @@ select * from t_3pas_in_one_inning;
 with
 three_pas_in_an_inning as ( -- get totals for each batter in their half inning of glory
 	  select game_id, inn_ct, bat_home_id, bat_id,
-	  	     sum(event_outs_ct) as outs_made, 
-	  	     count(case when ab_fl then 1 end) as ab,
-	  	     count(case when h_fl>0 then 1 end) as h, 
-	  	     sum(h_fl) as tb -- h_fl is really the count of TB, not a hit flag (ugh)
-	    from t_3pas_in_one_inning as t 
-	group by game_id, inn_ct, bat_home_id, bat_id -- same half inning, same batter
+             sum(event_outs_ct) as outs_made, 
+             count(case when ab_fl then 1 end) as ab,
+             count(case when h_fl>0 then 1 end) as h, 
+             sum(h_fl) as tb -- h_fl is really the count of TB, not a hit flag (ugh)
+        from t_3pas_in_one_inning as t 
+    group by game_id, inn_ct, bat_home_id, bat_id -- same half inning, same batter
 ),
 three_pas_in_an_inning_annotated as ( -- add the game_dt and teams
       select t.*, game_dt, away_team_id, home_team_id 
