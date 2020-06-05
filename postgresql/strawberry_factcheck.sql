6f3ea163e3982cfc03ff5fdad13bc026840dc155
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-06-04 21:03:40 -0700

    Code readability

--- postgresql/strawberry_factcheck.sql
+++ postgresql/strawberry_factcheck.sql
@@ -20,19 +20,19 @@ of the Atlanta Braves.[18]
 
 with 
 ph_slams as (select bat_id, game_id  from retrosheet_event where ph_fl is true and rbi_ct=4),
-multi_ph_slams as
-(
-	select bat_id, extract(year from game_dt) as yr, count(retrosheet_game.game_dt) as num 
-	from ph_slams
-	inner join retrosheet_game on ph_slams.game_id = retrosheet_game.game_id
-	group by bat_id, extract(year from game_dt)
-	having count(*)>1
+multi_ph_slam_seasons as
+(     select bat_id, extract(year from game_dt) as yr, count(retrosheet_game.game_dt) as num 
+        from ph_slams
+  inner join retrosheet_game 
+          on ph_slams.game_id = retrosheet_game.game_id
+    group by bat_id, extract(year from game_dt)
+      having count(*)>1
 )
-
-select p.name_first, p.name_last, bat_id, yr, num 
-from multi_ph_slams
+-- Now get the name and format the output
+    select p.name_first, p.name_last, bat_id, yr, num 
+      from multi_ph_slam_seasons
 inner join baseballdatabank_people as p
-on p.retro_id = multi_ph_slams.bat_id
+        on p.retro_id = multi_ph_slam_seasons.bat_id
 ;
 
 /* He had two three-home run games in his career, both of which came against Chicago teams 
