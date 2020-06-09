93abe7e533794b1dc2a2cd32a3f05bbdd8154bfb
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-06-09 16:06:04 -0700

    Code readability

--- postgresql/3000_hit_teammates.sql
+++ postgresql/3000_hit_teammates.sql
@@ -1,3 +1,10 @@
+-- Find games where multiple players who've already reached 3000 hits played together
+--   Inspired by Oct 7, 2001, when Rickey Henderson got his 3000th hit in Tony Gwynn's
+--   final game.  In the post-game ceremony, they said it was the first time in NL
+--   history that two teammates played in a game with 3000 hits.
+-- Retrosheet dailies include post-season and ASG.  AFAIK no way to filter to reg season
+--   So this will be a little over-inclusive.
+
 drop table t_3000h_players;
 
 create table t_3000h_players as
@@ -19,34 +26,36 @@ inner join baseballdatabank_people on players_with_3000.player_id=baseballdataba
 select * from t_3000h_players order by career_h desc;
 
 
--- dailies include post-season and ASGs. No way to filter (that I can figure out) to reg season
+-- compute the running hits total by game for all players with 3K
 drop table t_running_h_total;
 create table t_running_h_total as
 with
-dailies as
+dailies as -- select limited dailies for players with 3K
 (	select distinct player_id, game_id, game_dt, game_ct, team_id, b_h
 	from retrosheet_daily
 	where player_id in (select retro_id from t_3000h_players)
 )
-select player_id, game_id, team_id, game_dt, game_ct, sum(b_h) over
-			(partition by player_id order by game_dt, game_ct) as running_h_total
+select player_id, game_id, team_id, game_dt, game_ct,
+	-- this window function acts from the beginning of the partition to the current row,
+	-- so the sum(b_h) is a running total
+	sum(b_h) over (partition by player_id order by game_dt, game_ct) as running_h_total
 from dailies
 order by game_dt, game_ct
 ;
 
 -- verify we're not seeing double-counted games
--- total should be 3044 because of ASG and post-season
+-- total hits should be 3044 because of ASG and post-season (should be 2473 rows)
 select * from t_running_h_total where player_id='clemr101';
 
 -- see when each player crossed 3000 H
-select player_id, min(game_dt) as dt
+select player_id, min(game_dt) as dt_3000th
 from t_running_h_total
 where running_h_total>=3000
 group by player_id
-order by dt;
+order by dt_3000th;
 
 
--- Now find games with at least one player at 3000 H
+-- Enumerate each player's games once they'd reached 3000, and aggregate
 with
 games_with_3000K as
 (	select game_id, team_id, count(player_id) as num_in_gm, extract (year from game_dt) as yr
@@ -55,9 +64,9 @@ games_with_3000K as
 	group by game_id, team_id, game_dt
 )
 
-select team_id, yr, count(game_id) as num_games
+select team_id, yr, count(game_id) as num_games, max(num_in_gm) as max_3000s_in_gm
 from games_with_3000K
-where num_in_gm>2
+where num_in_gm>1
 group by team_id, yr
 order by yr;
 
