b528e1dcb6cf8dd59af8a49649137d282bbbfc94
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-06-10 16:55:01 -0700

    Simplified implementation

--- postgresql/3HRg_both_lgs.sql
+++ postgresql/3HRg_both_lgs.sql
@@ -2,7 +2,7 @@
 
 
 with
-thgs as
+thgs as -- all three-homer-games (THG)
 (       select distinct player_id, team_id, game_id from retrosheet_daily where b_hr>=3
 ),
 thgs_annotated as -- add the lg_id and game_dt
@@ -11,30 +11,28 @@ thgs_annotated as -- add the lg_id and game_dt
           from thgs as t
     inner join retrosheet_game as g on g.game_id=t.game_id
 ),
-thgs_lg_cts as
-(  -- can't do COUNT DISTINCT in a window function, so we have to do this two-step hack to add columns
-   -- to find the first 3HR-game hit in a new league
-        select *,
-               sum(new_lg) over (partition by player_id) as lg_ct
+thg_first_in_leagues as
+(  -- Find each player's first THG in each league (by numbering them by player/league first)
+        select *
           from (select a.*,
-                       (case when (row_number() over( partition by player_id, lg_id) = 1) then 1 else 0 end) as new_lg
+                       row_number() over( partition by player_id, lg_id order by game_dt) as thg_in_lg
                   from thgs_annotated as a
                ) x
+         where thg_in_lg=1
+
 ),
 players_both_lgs as
-(
-        select player_id, max(game_dt) as achieved_dt
-          from thgs_lg_cts
-         where lg_ct>1 and new_lg=1
+( -- Find players with two first-THG-in-a-league
+        select player_id, min(game_dt) as first_league_dt, max(game_dt) as second_league_dt
+          from thg_first_in_leagues
       group by player_id
+        having count(thg_in_lg)=2
 )
 
 -- Add the name and format the output
-    select achieved_dt, concat(p.name_first, ' ', p.name_last) as player
+    select first_league_dt, second_league_dt, concat(p.name_first, ' ', p.name_last) as player
       from players_both_lgs as pbl
 inner join baseballdatabank_people as p
         on p.retro_id=pbl.player_id
-  order by achieved_dt
-
-
+  order by second_league_dt
 ;
