10b7d051b82fce92861967b74ee1d2a3482b2a36
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-06-27 17:58:34 -0700

    Code cleanup

--- postgresql/best_60g_stretches.sql
+++ postgresql/best_60g_stretches.sql
@@ -5,9 +5,9 @@ create table t_60g_stretches as
 select player_id, game_dt as end_dt
      , row_number() over (partition by player_id order by game_dt, game_ct) as start_game_num
      , lag(game_dt, 59) over (partition by player_id order by game_dt, game_ct) as start_dt
-     , sum(b_h)  over (partition by player_id order by game_dt, game_ct asc rows 59 preceding) as h
-     , sum(b_ab) over (partition by player_id order by game_dt, game_ct asc rows 59 preceding) as ab
-     , sum(b_pa) over (partition by player_id order by game_dt, game_ct asc rows 59 preceding) as pa
+     , sum(b_h)    over (partition by player_id order by game_dt, game_ct asc rows 59 preceding) as h
+     , sum(b_ab)   over (partition by player_id order by game_dt, game_ct asc rows 59 preceding) as ab
+     , sum(b_pa)   over (partition by player_id order by game_dt, game_ct asc rows 59 preceding) as pa
      , count(b_pa) over (partition by player_id order by game_dt, game_ct asc rows 59 preceding) as gms
   from retrosheet_daily
  where extract(year from game_dt) >=1990
@@ -15,11 +15,10 @@ select player_id, game_dt as end_dt
 
 -- Find qualifying stretches, and compute BA
 with qual_stretches as
-(
-select *, h::decimal/ab as ba
-  from t_60g_stretches
- where gms=60
-   and pa>=3.1*60
+(      select *, h::decimal/ab as ba
+         from t_60g_stretches
+        where gms=60
+          and pa>=3.1*60
 )
 
 select s.player_id, concat(p.name_first, ' ', p.name_last), start_dt, end_dt, h, ab, pa, round(ba,4) as ba
