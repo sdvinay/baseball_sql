9dde6dabcb9fdf2c1b5cee7d1bc17b116767f237
Vinay Kumar <sdvinay@gmail.com>
2022-05-24 11:33:20 -0700

    minor code cleanup

--- pandas/baseball_stats_utils.py
+++ pandas/baseball_stats_utils.py
@@ -42,7 +42,7 @@ def summarize_events(pa, groupby):
     event_cd_mapper = {3: 'k', 14: 'bb', 15: 'ibb', 23: 'hr'}
     counts = groups['event_cd'].value_counts().unstack()[event_cd_mapper.keys()].rename(columns=event_cd_mapper)
     for col in counts.columns:
-        counts[col] = counts[col].fillna(0).apply(lambda x: int(x))
+        counts[col] = counts[col].fillna(0).astype(int)
 
     # Combine and add rate stats
     stats = pd.concat([sums, counts], axis=1)
