a3ad8694f0e6e27b970e0b4b93e9c44480ab4e09
Vinay Kumar <sdvinay@gmail.com>
2022-09-08 13:39:28 -0700

    Use typer to parameterize num_trials

--- pandas/playoff_odds/season_simulator.py
+++ pandas/playoff_odds/season_simulator.py
@@ -1,7 +1,13 @@
+import typer
+
 import playoff_odds as po
 
-(played, remain) = po.get_games()
-cur_standings = po.compute_standings(played)
-sim_results = po.sim_n_seasons(cur_standings, remain, 100)
-summary = po.summarize_sim_results(sim_results)
-print(summary.sort_values('champ_shares', ascending=False).to_string())
+def main(num_trials: int = 100):
+    (played, remain) = po.get_games()
+    cur_standings = po.compute_standings(played)
+    sim_results = po.sim_n_seasons(cur_standings, remain, num_trials)
+    summary = po.summarize_sim_results(sim_results)
+    print(summary.sort_values('champ_shares', ascending=False).to_string())
+
+if __name__ == "__main__":
+    typer.run(main) 
