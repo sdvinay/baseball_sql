4f5354c0353531a1df9193ec460740a0f83e7f67
Vinay Kumar <sdvinay@gmail.com>
2022-09-22 15:25:26 -0700

    Compute WS shares based on playoff fields and ratings rather than using seed-based weights

--- pandas/playoff_odds/summarize_results.py
+++ pandas/playoff_odds/summarize_results.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
+import season_simulator as sim
+import playoff_simulator as psim
 
 # Weight each playoff seed, for various purposes
 weights = {}
-# Championship weights by seed position
-weights['champ_shares'] = dict(enumerate([.135, .13, .07, .065, .05, .05], 1))
 # Home-game likelihood.  Top 4 seeds get home games, bottom two have to win the wild card series
 weights['home_game'] = dict(enumerate([1, 1, 1, 1, .44, .44], 1))
 
@@ -22,4 +22,49 @@ def summarize_sim_results(df_results):
     # Generate a column for each set of weights defined
     for col in weights.keys():
         summary[col] = (summary[range(1,7)] * np.array(weights[col])).sum(axis=1)
-    return summary
+
+    (wins, lg_ranks, tms_by_rank, wins_by_rank) = restructure_results(df_results)
+    cur, remain = sim.get_games()
+    ratings = remain[['team1', 'rating1_pre']].drop_duplicates().set_index('team1')['rating1_pre'].rename('rating').sort_values(ascending=False)
+
+    pennant_shares = compute_pennant_shares(tms_by_rank, ratings)
+    ws_shares = compute_ws_shares(pennant_shares, ratings, summary['mean'])
+    summary['ws_shares'] = pd.Series(ws_shares)
+    summary['ws_shares'] = summary['ws_shares'].fillna(0)
+    
+    return summary.sort_values(['ws_shares', 'mean'], ascending=[False, False])
+
+def restructure_results(sim_results):
+    wins = sim_results['W'].unstack(level='team')
+    lg_ranks = sim_results['lg_rank'].unstack(level='team')
+    tms_by_rank = sim_results[['lg', 'lg_rank']].reset_index().set_index(['run_id', 'lg', 'lg_rank'])['team'].unstack(level='lg_rank')
+    wins_by_rank = sim_results.reset_index().set_index(['run_id', 'lg', 'lg_rank'])['W'].unstack(level='lg_rank')
+    return (wins, lg_ranks, tms_by_rank, wins_by_rank)
+
+
+# Compute pennant shares for all teams, by iterating over the possible playoff fields
+def compute_pennant_shares(tms_by_rank, ratings):
+    def compute_pennant_prob(field):
+        seeds = dict(enumerate(field.name, 1))
+        return pd.Series(psim.run_league(ratings, seeds))*field["count"]
+
+    pennant_shares = {}
+    for lg in ('N', 'A'):
+        fields = tms_by_rank.query("lg==@lg").loc[:,1:6].value_counts().rename("count")
+        pennant_shares[lg]  = pd.DataFrame(fields).apply(compute_pennant_prob, axis=1).fillna(0).sum()
+
+    return pennant_shares
+
+
+def compute_ws_shares(pennant_shares, ratings, wins):
+    ws_shares = {}
+    for a in pennant_shares['A'].index:
+        for n in pennant_shares['N'].index:
+            likelihood = pennant_shares['A'][a]/pennant_shares['A'].sum() * pennant_shares['N'][n]
+            key_func = lambda tm: wins[tm]
+            tms = sorted([a, n], key=key_func, reverse=True)
+            p = psim.p_series7(ratings[tms[0]], ratings[tms[1]])
+            ws_shares[tms[0]] = ws_shares.get(tms[0], 0) + (p * likelihood)
+            ws_shares[tms[1]] = ws_shares.get(tms[1], 0) + ((1-p) * likelihood)
+
+    return ws_shares
