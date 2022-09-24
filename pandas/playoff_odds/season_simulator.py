c65dab7cea515b5fc968fab29e4ae468f8d85302
Vinay Kumar <sdvinay@gmail.com>
2022-09-24 15:24:57 -0700

    Randomly vary each team's rating in each job

--- pandas/playoff_odds/season_simulator.py
+++ pandas/playoff_odds/season_simulator.py
@@ -137,12 +137,15 @@ def compute_probs(gms, ratings):
     rating2 = pd.merge(left=gms, right=ratings, left_on='team2', right_index=True, how='left')['rating']
     return ssim.p_game(rating1, rating2)    
     
+def vary_ratings(ratings):
+    offsets = (-100, 100, 0, 0)
+    return ratings + np.random.choice(offsets, len(ratings))
 
 def main(num_seasons: int = 100, save_output: bool = False, save_summary: bool = True, save_ranks: bool = True, id: int = 0, show_summary: bool = True):
     print(f'Simulating {num_seasons} seasons as ID {id}')
     (played, remain) = ds.get_games()
     cur_standings = compute_standings(played)
-    ratings = ds.get_ratings()
+    ratings = vary_ratings(ds.get_ratings())
     remain['win_prob'] = compute_probs(remain, ratings)
 
     sim_results = sim_n_seasons(remain, num_seasons)
