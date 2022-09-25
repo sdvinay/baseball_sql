91493549238489112533560938a5b36e041f9d9a
Vinay Kumar <sdvinay@gmail.com>
2022-09-25 15:11:56 -0700

    move league structure into data source

--- pandas/playoff_odds/datasource_538.py
+++ pandas/playoff_odds/datasource_538.py
@@ -30,3 +30,24 @@ def get_games():
 def get_ratings():
     return ratings
 
+
+# This is the source data for the mapping of teams to divisions/leagues
+def get_league_structure_impl():
+    div_text = '''
+    NLW: ARI COL LAD SDP SFG
+    NLE: ATL FLA NYM PHI WSN
+    ALW: SEA ANA HOU OAK TEX
+    ALE: TBD TOR BAL NYY BOS
+    ALC: MIN CHW CLE KCR DET
+    NLC: STL MIL CHC PIT CIN
+    '''
+
+    lines = [line.strip().split(': ') for line in div_text.strip().split('\n')]
+    div_map = {l[0]: l[1].split(' ') for l in lines} # a map of div names to lists of team names
+    div = pd.concat([pd.Series({team: div for team in teams}) for (div, teams) in div_map.items()])
+    teams = pd.DataFrame()
+    teams['div'] = div
+    teams['lg'] = teams['div'].str[0]
+    return teams
+
+league_structure = get_league_structure_impl()
--- pandas/playoff_odds/season_simulator.py
+++ pandas/playoff_odds/season_simulator.py
@@ -35,7 +35,7 @@ def process_sim_results(sim_results):
     sim_results['wpct'] = sim_results['W'] / (sim_results['W'] + sim_results['L'])
 
     # Merge in the div/lg data
-    sim_results = pd.merge(left=sim_results, right=league_structure, left_on='team', right_index=True)
+    sim_results = pd.merge(left=sim_results, right=ds.league_structure, left_on='team', right_index=True)
     sim_results = sim_results.set_index(['run_id', 'team'])[['W', 'L', 'wpct', 'div', 'lg']]
 
     # compute div_wins and playoff seeds
@@ -94,27 +94,6 @@ def add_lg_ranks(sim_results):
     sim_results['lg_rank'] = sim_results.sort_values(by=['div_win', 'wpct', 'tiebreak'], ascending=False).groupby(['run_id', 'lg']).cumcount()+1
     return sim_results
 
-
-# This is the source data for the mapping of teams to divisions/leagues
-def get_league_structure():
-    div_text = '''
-    NLW: ARI COL LAD SDP SFG
-    NLE: ATL FLA NYM PHI WSN
-    ALW: SEA ANA HOU OAK TEX
-    ALE: TBD TOR BAL NYY BOS
-    ALC: MIN CHW CLE KCR DET
-    NLC: STL MIL CHC PIT CIN
-    '''
-
-    lines = [line.strip().split(': ') for line in div_text.strip().split('\n')]
-    div_map = {l[0]: l[1].split(' ') for l in lines} # a map of div names to lists of team names
-    div = pd.concat([pd.Series({team: div for team in teams}) for (div, teams) in div_map.items()])
-    teams = pd.DataFrame()
-    teams['div'] = div
-    teams['lg'] = teams['div'].str[0]
-    return teams
-
-league_structure = get_league_structure()
 
 
 def sim_n_seasons(games, n):
